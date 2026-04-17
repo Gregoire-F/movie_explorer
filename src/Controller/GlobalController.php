962cecd09d3926011f54d64a52a300e69ae872c6
Gregoire-F <gregoire.frou@gmail.com>
2026-04-17 16:30:16 +0200

    Fin

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -18,7 +18,7 @@ class GlobalController extends AbstractController
     #[Route('/liste', name: 'app_liste')]
 
     public function liste(): Response
-    {   
+    {
         $films = [
             'Les visiteurs 1, 1993',
             'Tais-toi, 2006',
@@ -26,6 +26,25 @@ class GlobalController extends AbstractController
         ];
         return $this->render('liste.html.twig', ['titre' => 'Liste des films', 'films' => $films]);
     }
+
+    #[Route('/film/{id}', name: 'app_film_id')]
+    public function filmId(int $id): Response
+
+    {
+        return new Response('Film n°' . $id);
+    }
+
+    #[Route('api/films/', name: 'app_film')]
+    public function apiFilms(): Response
+    {
+        $films = [
+            ['id' => 1, 'titre' => 'Les visiteurs 1', 'annee' => 1993],
+            ['id' => 2, 'titre' => 'Tais-toi', 'annee' => 2006],
+            ['id' => 3, 'titre' => 'Django Unchained', 'annee' => 2012]
+        ];
+        return $this->render('films.html.twig', ['films' => $films]);
+    }
+
     #[Route('/contact', name: 'app_contact')]
     public function contact(): Response
     {
