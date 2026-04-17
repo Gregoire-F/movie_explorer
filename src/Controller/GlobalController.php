cf26c87974a871a5e79f6ca17562bda55c466b14
Gregoire-F <gregoire.frou@gmail.com>
2026-04-17 15:18:28 +0200

    1 titre dynamique

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -7,20 +7,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class GlobalController extends AbstractController
+
 {
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', ['titre' => 'Bienvenue sur Movie Explorer']);
     }
 
     #[Route('/liste', name: 'app_liste')]
-    public function liste (): Response
+
+    public function liste(): Response
     {
-        return $this->render('liste.html.twig');
+        return $this->render('liste.html.twig', ['titre' => 'Liste des films']);
     }
     #[Route('/contact', name: 'app_contact')]
-    public function contact (): Response
+    public function contact(): Response
     {
         return $this->render('contact.html.twig');
     }
