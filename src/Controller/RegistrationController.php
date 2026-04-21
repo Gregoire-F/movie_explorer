e835ca9080868a4a22549b532bf2284941107c86
Gregoire-F <gregoire.frou@gmail.com>
2026-04-21 15:39:03 +0200

    mail par default changé

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -43,7 +43,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('gr.frou@o3experts.fr', 'Greg'))
+                    ->from(new Address('greg@greg.fr', 'Greg'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
